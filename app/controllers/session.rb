14c971048dfdfbf45a9ee17b0134f3d02df73977
alwinsolanky <alwinsolanky@gmail.com>
2016-08-09 12:02:45 +0100

    user signup implemented, need to merge with backend

--- app/controllers/session.rb
+++ app/controllers/session.rb
@@ -2,4 +2,16 @@ class MakersBnb < Sinatra::Base
   get '/' do
     erb :index
   end
+
+  post '/session/create' do
+    user = User.create(email: params[:email],
+      password: params[:password])
+    if user.save
+      session[:user_id] = user.id
+      redirect to('/')
+    else
+       erb :index
+    end
+  end
+
 end
