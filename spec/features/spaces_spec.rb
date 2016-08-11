3bacab68f5945bb181aac547372f92a6ed1f55f5
Zeeshan Ishtiaq <mzishtiaq@yahoo.co.uk>
2016-08-11 12:16:41 +0100

    adds available from and available to dates

--- spec/features/spaces_spec.rb
+++ spec/features/spaces_spec.rb
@@ -13,11 +13,13 @@ feature 'spaces' do
     expect(page.current_path).to eq('/spaces/new')
   end
 
-  scenario 'user can list space' do
+  scenario 'user can list space with available to and from dates' do
     click_button('List a Space')
     fill_in :name,          with: 'Beautiful relaxing space'
     fill_in :description,   with: 'Feel at home in this beautiful townhouse'
     fill_in :price,         with:  25
+    fill_in :date_from,     with:  "11/08/2016"
+    fill_in :date_to,       with:  "16/08/2016"
     click_button('List my Space')
     expect(page).to have_content('Beautiful relaxing space Feel at home in this beautiful townhouse')
   end
