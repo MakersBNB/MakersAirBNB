570db763493a530e98dd38a21c08dacd13bf449f
Manjinder Gill <manjinder.s.gill@googlemail.com>
2016-08-12 23:17:19 +0100

    Implemented host approval, all tests passing

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,6 +4,7 @@ class Booking
   property :id, Serial
   property :booked_from, DateTime
   property :booked_to, DateTime
+  property :confirmed, Boolean, :default => false
 
   belongs_to :user
   belongs_to :space
