abc897dc69169edf1b7090f3a980575cb226720d
Zeeshan Ishtiaq <mzishtiaq@yahoo.co.uk>
2016-08-12 17:55:03 +0100

    passes the booking test #43

--- spec/features/bookings_spec.rb
+++ spec/features/bookings_spec.rb
@@ -1,16 +1,12 @@
 feature 'Bookings' do
-
   scenario 'User can book a space', :js=>true do
     sign_up(email: 'k@test.com', password: 'test', password_confirmation: 'test')
     login(email: 'k@test.com', password: 'test')
-    visit '/'
-    click_link 'Spaces'
-    click_link 'Relaxing space by the beach'
-    within_table('calendar') do
-      find(:xpath, "//tr/td", :text => '10').click
-      find(:xpath, "//tr/td", :text => '12').click
-    end
+    create_space
+    visit '/spaces'
+    click_link 'space-link'
+    click_link '18'
     click_button 'Request to book'
-    expect(page).to have_content 'Your booking has been recieved'
+    expect(page).to have_content 'Your booking request has been sent'
   end
 end
