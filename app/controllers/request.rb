093e1348c0271960662445f4ba790325328576f7
Manjinder Gill <manjinder.s.gill@googlemail.com>
2016-08-12 14:34:57 +0100

    Implemented post request of single night bookings, data is being added to db

--- app/controllers/request.rb
+++ app/controllers/request.rb
@@ -1,14 +1,16 @@
 class MakersBnb < Sinatra::Base
 
-  post '/request/new' do
-    booking = Booking.create(booked_from: params[:selectedFrom],
-                              booked_to: params[:selectedTo])
+  post '/request/new/:id' do |i|
+    booking = Booking.create(user_id: session[:user_id],
+                              booked_from: params[:selectedFrom],
+                              booked_to: params[:selectedTo],
+                              space_id: params[:id])
     if booking.save
       flash.keep[:notice] = "Your booking request has been sent"
       redirect '/spaces'
     else
-      flash.keep[:error] = "Try again, your request couldn't be sent."
-      redi
+      flash.keep[:error] = booking.errors.full_messages
+      redirect '/spaces/' + params[:id]
     end
   end
 
